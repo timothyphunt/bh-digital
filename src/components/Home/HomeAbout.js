64930503b81055b18527ae149ddfd1491afde081
Adam <adampugh@zoho.com>
2019-05-26 23:41:19 +0100

    removed whitespace

--- src/components/Home/HomeAbout.js
+++ src/components/Home/HomeAbout.js
@@ -11,7 +11,6 @@ const HomeAbout = () => (
                     <h2 className="subtitle">About Us</h2>
                     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint iusto dolor dolores molestiae porro delectus voluptatum aliquam, a consequatur dolore, similique culpa possimus rem saepe, deleniti tempora. Asperiores, recusandae magni?</p>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi aliquam corrupti, similique repellendus consequatur tempore facere vero, quo ab saepe vitae sed officia. Rerum et, facilis nulla eos nihil delectus aut ipsam est nam corrupti eligendi officia assumenda. Rerum, porro!</p>
-                    
                     <button className="btn btn--blue">contact us</button>
                 </div>
             </div>
