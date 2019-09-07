a1a397b6329759e44c12133980b08190475f8330
Adam <adampugh@zoho.com>
2019-09-07 14:36:18 +0100

    Style updates

--- src/components/Services/ServicesAbout.js
+++ src/components/Services/ServicesAbout.js
@@ -39,8 +39,14 @@ const ServicesAbout = () => (
             </div>
         </div>
 
-        <div className="container">
-            <div className="servicesAbout__service">
+        <svg className="servicesAbout__wavyDiv" id="" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1416.99 174.01">
+            <defs></defs><title></title>
+            <path className="cls-1" d="M0,280.8S283.66,59,608.94,163.56s437.93,150.57,808,10.34V309.54H0V280.8Z"
+            transform="translate(0 -135.53)" />
+        </svg>
+
+        <div className="servicesAbout__service--grey">
+            <div className="container servicesAbout__service">
                 <h1 className="title">PPC</h1>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, quidem. Unde officia rerum modi quaerat vel libero sunt. Commodi, ut? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut, ducimus.</p>   
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, saepe? Error obcaecati doloremque repudiandae dolor quos! Quasi voluptas, ipsa distinctio repudiandae sit ex non soluta ab odio ratione, tempore accusantium quos perspiciatis deleniti voluptatum consequuntur ducimus. Obcaecati architecto earum molestiae?</p>   
@@ -67,6 +73,12 @@ const ServicesAbout = () => (
                 </ScrollAnimation>
             </div>
         </div>
+
+        <svg className="servicesAbout__wavyDiv--white" id="" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1416.99 174.01">
+            <defs></defs><title></title>
+            <path className="cls-1" d="M0,280.8S283.66,59,608.94,163.56s437.93,150.57,808,10.34V309.54H0V280.8Z"
+            transform="translate(0 -135.53)" />
+        </svg>
 
         <div className="container">
             <div className="servicesAbout__service">
@@ -97,8 +109,14 @@ const ServicesAbout = () => (
             </div>
         </div>
 
-        <div className="container">
-            <div className="servicesAbout__service">
+        <svg className="servicesAbout__wavyDiv" id="" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1416.99 174.01">
+            <defs></defs><title></title>
+            <path className="cls-1" d="M0,280.8S283.66,59,608.94,163.56s437.93,150.57,808,10.34V309.54H0V280.8Z"
+            transform="translate(0 -135.53)" />
+        </svg>
+
+        <div className="servicesAbout__service--grey servicesAbout--last">
+            <div className="container servicesAbout__service">
                 <h1 className="title">Content</h1>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, quidem. Unde officia rerum modi quaerat vel libero sunt. Commodi, ut? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut, ducimus.</p>   
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, saepe? Error obcaecati doloremque repudiandae dolor quos! Quasi voluptas, ipsa distinctio repudiandae sit ex non soluta ab odio ratione, tempore accusantium quos perspiciatis deleniti voluptatum consequuntur ducimus. Obcaecati architecto earum molestiae?</p>   
@@ -125,14 +143,6 @@ const ServicesAbout = () => (
                 </ScrollAnimation>
             </div>
         </div>
-
-        
-
-        <svg className="servicesAbout__wavyDiv" id="" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1416.99 174.01">
-            <defs></defs><title></title>
-            <path className="cls-1" d="M0,280.8S283.66,59,608.94,163.56s437.93,150.57,808,10.34V309.54H0V280.8Z"
-            transform="translate(0 -135.53)" />
-        </svg>
     </section>
 );
 
