9927bd260c77cb18e430cb9db6ed03ed6ae7964d
timothyphunt <57625639+timothyphunt@users.noreply.github.com>
2019-11-25 00:51:43 +0000

    Update HomeClients.js

--- src/components/Home/HomeClients.js
+++ src/components/Home/HomeClients.js
@@ -3,7 +3,7 @@ import ScrollAnimation from 'react-animate-on-scroll';
 
 import LockieLtd from '../../../static/assets/lockie-ltd.jpg';
 import DiageoLtd from '../../../static/assets/diageo.jpg';
-import JWPLtd from '../../../static/assets/jwproducts-logo.png';
+import JWPLtd from '../../../static/assets/jw-products-logo.jpg';
 
 class HomeClients extends Component {
     render() {
@@ -31,7 +31,7 @@ class HomeClients extends Component {
                         <ScrollAnimation animateIn="fadeIn" animateOnce={true}>
                             <div className="homeClients__grid__client">
                                 <div className="homeClients__grid__client__info">
-                                    <img src={LockieLtd} alt="Lockie Ltd" />
+                                     <a href="www.lockiechurch.com"> <img src={LockieLtd} alt="Lockie Ltd" /></a>
                                     <div className="homeClients__grid__client__info__text">
                                         <h3 className="subtitle">Bob Pooley</h3>
                                         <p>MD, <b>Lockie Limited</b></p>
@@ -45,7 +45,7 @@ class HomeClients extends Component {
                         <ScrollAnimation animateIn="fadeIn" animateOnce={true}>
                             <div className="homeClients__grid__client">
                                 <div className="homeClients__grid__client__info">
-                                    <a href="diageo.com"> <img src={DiageoLtd} alt="Diageo" /></a>
+                                    <a href="www.diageo.com"> <img src={DiageoLtd} alt="Diageo" /></a>
                                     <div className="homeClients__grid__client__info__text">
                                         <h3 className="subtitle">Eddie Simpkins</h3>
                                         <p>NA Executive, <b>Diageo</b></p>
