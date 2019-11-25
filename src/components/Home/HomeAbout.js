c7096805eb7f95454edee900a46511af5d637459
timothyphunt <57625639+timothyphunt@users.noreply.github.com>
2019-11-25 13:23:22 +0000

    Update HomeAbout.js

--- src/components/Home/HomeAbout.js
+++ src/components/Home/HomeAbout.js
@@ -11,7 +11,7 @@ const HomeAbout = () => (
                 <div className="homeAbout__content__text">
                     <h2 className="subtitle">About Us</h2>
                     <p>Bluehorn Digital is a Liverpool-based, full-service marketing agency with dedicated professionals and specialist knowledge in web design, digital marketing, paid search and SEO strategies. Using a creative and analytical approach to marketing, we help companies of all sizes maximise their online earning potential and develop their brand.</p>
-                    <p>Here at Bluehorn, we employ a research-based approach, allowing for a tailored marketing and brand development plan for your company. We are based in Liverpool and frequently visit clients throughout the North West area as part of a hands-on approach to develop effective digital strategies for your company.</p>
+                    <p>Here at Bluehorn, we employ a research-based approach, allowing for a tailored marketing and brand development plan for your company. We work closely with our clients so that we can understand their vision and bring it to life. To find out how we can help your company reach its full potential, get in touch with us today.</p>
                   <button className="btn btn--blue">Contact Us</button>
                 </div>
             </div>
