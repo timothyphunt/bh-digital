f9b3510c931d50521fea7f7281ad2b5d27382d3c
timothyphunt <57625639+timothyphunt@users.noreply.github.com>
2019-12-17 07:25:00 +0000

    Add files via upload

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,19 @@ module.exports = {
     },
     `gatsby-transformer-remark`,
     // allows gatsby v1 to use react v16
-    `gatsby-plugin-react-next`
+    `gatsby-plugin-react-next`,
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Bluehorn Digital`,
+        short_name: `Bluehorn`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+        icon: 'static/assets/favicon.png'
+      },
+    }
   ],
 }
