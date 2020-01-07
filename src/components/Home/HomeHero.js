409999865ad942cc5c940939d6a411123cd4c690
timothyphunt <57625639+timothyphunt@users.noreply.github.com>
2020-01-07 14:16:44 +0000

    Add files via upload

--- src/components/Home/HomeHero.js
+++ src/components/Home/HomeHero.js
@@ -35,9 +35,3 @@ class HomeHero extends Component {
 }
 
 export default HomeHero;
-
-// <svg id="" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1416.99 174.01">
-//     <defs><style>.cls-1{fill:#74c044;}</style></defs><title>Untitled-4</title>
-//     <path className="cls-1" d="M0,280.8S283.66,59,608.94,163.56S437.93,150.57,808,10.34V309.54H0V280.8Z"
-//     transform="translate(0-135.53)"/>
-// </svg>
