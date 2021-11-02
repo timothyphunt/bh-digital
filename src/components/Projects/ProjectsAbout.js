2c653d2ac0f707895c94f690ed126b3819c12eb0
timothyphunt <57625639+timothyphunt@users.noreply.github.com>
2021-11-02 13:03:20 +0000

    Update ProjectsAbout.js

--- src/components/Projects/ProjectsAbout.js
+++ src/components/Projects/ProjectsAbout.js
@@ -139,7 +139,7 @@ const ProjectsAbout = () => (
         </div>
 
     <div className="projectsAbout__project" id="lockie">
-            <div className="projectsAbout__project__hero" id="lockie">
+            <div className="projectsAbout__project__hero" id="lockie-hero">
                 <svg className="projectsAbout__project__wavyDiv--top" id="" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1416.99 174.01">
                     <defs></defs><title></title>
                     <path className="cls-1" d="M0,280.8S283.66,59,608.94,163.56s437.93,150.57,808,10.34V309.54H0V280.8Z"
